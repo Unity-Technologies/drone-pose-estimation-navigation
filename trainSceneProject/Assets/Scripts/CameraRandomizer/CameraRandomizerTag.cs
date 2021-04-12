d868f7e911cc049214ce414f69a554985ba74223
Jonathan Leban <jonathan.leban@jonathan.leban-406M>
2021-04-12 13:34:09 -0400

    change the camera randomizer

---
+++ trainSceneProject/Assets/Scripts/CameraRandomizer/CameraRandomizerTag.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Perception.Randomization.Randomizers;
+using UnityEngine.Perception.Randomization.Parameters;
+
+[AddComponentMenu("Perception/RandomizerTags/Camera Randomizer Tag")]
+public class CameraRandomizerTag : RandomizerTag
+{
+    [HideInInspector]
+    public Vector3 rootPosePosition;
+    
+    [HideInInspector]
+    public Vector3 rootPoseRotation;
+
+    private void Start()
+    {
+        rootPosePosition = transform.position;
+        rootPoseRotation = transform.eulerAngles;
+    }
+
+}
--- trainSceneProject/Assets/Scripts/CameraRandomizer/CustomCameraRandomizerTag.cs
+++
@@ -1,6 +0,0 @@
-﻿using UnityEngine;
-using UnityEngine.Perception.Randomization.Randomizers;
-
-[RequireComponent(typeof(Camera))]
-[AddComponentMenu("Perception/RandomizerTags/Custom Camera Randomizer Tag")]
-public class CustomCameraRandomizerTag : RandomizerTag { }
