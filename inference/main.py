11573be26332e2d59cf093ba499a09957a50597d
Souranil Sen <souranil@unity3d.com>
2021-08-30 12:54:58 -0700

    fixing test

--- inference/main.py
+++ inference/main.py
@@ -2,14 +2,14 @@ import torch
 import torchvision
 import yaml
 from easydict import EasyDict
-
 from Model.pose_estimation.pose_estimation_estimator import PoseEstimationEstimator
 from Model.pose_estimation.drone_cube_dataset import DroneCubeDataset
 from Model.pose_estimation.model import PoseEstimationNetwork
 from Model.pose_estimation.evaluate import evaluation_over_batch
 
+
 class Inference:
-    def __init__(self, ckpt, transforms, estimator, scale_translation=1):
+    def __init__(self, ckpt, transforms=None, estimator=None, scale_translation=1):
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         self.device = "cpu"
         ckpt = torch.load(ckpt, map_location=self.device)
@@ -17,7 +17,7 @@ class Inference:
             self.transforms = self.get_default_transform()
         else:
             self.transforms = transforms
-            self.model = PoseEstimationNetwork(scale_translation)
+        self.model = PoseEstimationNetwork(scale_translation)
         self.model.load_state_dict(ckpt["model"])
         self.estimator = estimator
         self.config = estimator.config
@@ -27,14 +27,14 @@ class Inference:
             config=self.config,
             data_root=estimator.data_root,
             split="train",
-            zip_file_name=self.config.train.dataset_zip_file_name_training,
+            zip_file_name=self.config.test.dataset_zip_file_name_test,
         )
 
         self.test_loader = torch.utils.data.DataLoader(
             dataset_test,
-            batch_size=self.config.train.batch_training_size,
+            batch_size=self.config.test.batch_test_size,
             num_workers=0,
-            drop_last=True,
+            drop_last=False,
         )
         self.model.eval()
 
@@ -60,23 +60,25 @@ class Inference:
     def test(self):
         metric_translation_drone = 0
         metric_translation_cube = 0
+        len_data_loader = len(self.test_loader)
         for index, (images, target_t_drone_list, target_t_cube_list) in enumerate(self.test_loader):
             images = list(image.to(self.device) for image in images)
-
+            # T1
             output_t_drone, output_t_cube = self.model(
                 torch.stack(images).reshape(-1, 3, 224, 224)
                 )
-            
+            #T2
+            #T2-T1
             target_t_drone = target_t_drone_list.to(self.device)
             target_t_cube = target_t_cube_list.to(self.device)
-        print("wefwefwe")
-        metric_translation_cube += self.criterion(output_t_cube, target_t_cube)
-        metric_translation_drone += self.criterion(output_t_drone, target_t_drone)
+            print("wefwefwe")
+            metric_translation_cube += self.criterion(output_t_cube, target_t_cube)
+            metric_translation_drone += self.criterion(output_t_drone, target_t_drone)
 
-        print(f"Cube loss : {metric_translation_cube}, Drone loss: {metric_translation_drone}")
+            print(f"Cube loss : {metric_translation_cube}, Drone loss: {metric_translation_drone}")
 
-        avg_loss = (metric_translation_drone + metric_translation_cube) / (index + 1)
-        print(f"Avg Loss : {avg_loss}")
+            avg_loss = (metric_translation_drone + metric_translation_cube) / (index + 1)
+            print(f"Avg Loss : {avg_loss}")
 
             
 
@@ -90,12 +92,13 @@ class Inference:
 
 
 def get_config(filepath):
-    config = yaml.load(open(filepath, "r"), Loader=yaml.FullLoader)
+    f = open(filepath, 'r')
+    config = yaml.load(f, Loader=yaml.FullLoader)
     config = EasyDict(config)
     return config
 
 def test_model():
-    config = get_config("Model/config-local.yml")
+    config = get_config("/Users/souranil/ai/hackathon/ai-2021/ai-hw21-pose-estimation-model/Model/config-local.yml")
     estimator = PoseEstimationEstimator(config=config)
     transforms = torchvision.transforms.Compose(
             [
@@ -110,7 +113,7 @@ def test_model():
         )
 
     i = Inference(
-        "inference/drone_target_pose_estimation_ep4.tar",
+        "/Users/souranil/ai/hackathon/ai-2021/ai-hw21-pose-estimation-model/inference/drone_target_pose_estimation_ep50.tar",
         transforms=transforms,
         estimator=estimator
     )
