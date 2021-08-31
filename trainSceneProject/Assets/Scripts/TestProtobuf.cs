602392a321bc59e247bf2aa165552f5e7a0353dd
Priyesh Wani <priyesh@unity3d.com>
2021-08-30 22:26:08 -0500

    Updating screencapture

--- trainSceneProject/Assets/Scripts/TestProtobuf.cs
+++ trainSceneProject/Assets/Scripts/TestProtobuf.cs
@@ -11,6 +11,7 @@ using UnityEngine.Perception.Randomization.Scenarios;
 using Quaternion = UnityEngine.Quaternion;
 
 using System.Numerics;
+using Unity.Simulation;
 using UnityEngine.AI;
 using Vector3 = UnityEngine.Vector3;
 
@@ -27,6 +28,8 @@ public class TestProtobuf : MonoBehaviour
     public GameObject drone;
     public GameObject target;
 
+    public GameObject _canvas;
+
 
     // navmesh stuff
     public NavMeshSurface Surface;
@@ -40,6 +43,8 @@ public class TestProtobuf : MonoBehaviour
     // post process volume
     public CustomModifyPostProcessVolume ppv;
 
+    private bool captureDone = false;
+
 
     void Start()
     {
@@ -47,25 +52,54 @@ public class TestProtobuf : MonoBehaviour
         _poseEstimationClient = new PostEstimationService.PostEstimationServiceClient(_channel);
     }
 
-    private byte[] CaptureScreenshot()
+    public IEnumerator StartPoseEstimationInternal()
     {
-        Camera.main.targetTexture = renderTexture;
-        RenderTexture currentRT = RenderTexture.active;
-        RenderTexture.active = renderTexture;
-        Camera.main.Render();
-        Texture2D mainCameraTexture = new Texture2D(renderTexture.width, renderTexture.height);
-        mainCameraTexture.ReadPixels(new Rect(0, 0, renderTexture.width, renderTexture.height), 0, 0);
-        mainCameraTexture.Apply();
-        RenderTexture.active = currentRT;
-        // Get the raw byte info from the screenshot
-        byte[] imageBytes = mainCameraTexture.GetRawTextureData();
-        var encodedImageBytes = ImageConversion.EncodeArrayToPNG(imageBytes, GraphicsFormat.R8G8B8A8_UNorm,
-            (uint) renderTexture.width, (uint) renderTexture.height);
+        _canvas.SetActive(false);
+        yield return new WaitForEndOfFrame();
+        
+        var tex2D = new Texture2D(cam.pixelWidth, cam.pixelHeight);
+        RenderTexture.active = cam.targetTexture;
+        var rect = new Rect(0,0,cam.pixelWidth, cam.pixelHeight);
+        tex2D.ReadPixels(rect,0,0);
+        tex2D.Apply();
+        RenderTexture.active = null;
+
+        var data = tex2D.GetRawTextureData();
+        var encodedImageBytes = ImageConversion.EncodeArrayToPNG(data, GraphicsFormat.R8G8B8A8_UNorm,
+            (uint) cam.pixelWidth, (uint) cam.pixelHeight);
         var path = Application.persistentDataPath + "Image_" + _seq + ".png";
         _seq++;
         File.WriteAllBytes(path, encodedImageBytes);
-        Camera.main.targetTexture = null;
-        return imageBytes;
+
+        var poseEstimates = GetPoseEstimation(data);
+        targetEstimatedPosition = poseEstimates.TargetTransformPosition;
+        droneEstimatedPosition = poseEstimates.DroneTransformPosition;
+        Debug.Log("Done With capture..");
+        _canvas.SetActive(true);
+    }
+
+    public void StartPoseEstimation()
+    {
+        // Camera.main.targetTexture = renderTexture;
+        // RenderTexture currentRT = RenderTexture.active;
+        // RenderTexture.active = renderTexture;
+        // Camera.main.Render();
+        StartCoroutine(StartPoseEstimationInternal());
+        
+        // Texture2D mainCameraTexture = new Texture2D(renderTexture.width, renderTexture.height);
+        // mainCameraTexture.ReadPixels(new Rect(0, 0, renderTexture.width, renderTexture.height), 0, 0);
+        // mainCameraTexture.Apply();
+        // RenderTexture.active = currentRT;
+        // // Get the raw byte info from the screenshot
+        // byte[] imageBytes = mainCameraTexture.GetRawTextureData();
+        // var encodedImageBytes = ImageConversion.EncodeArrayToPNG(imageBy       // vartes, GraphicsFormat.R8G8B8A8_UNorm,
+        //     (uint) renderTexture.width, (uint) renderTexture.height);
+        // var path = Application.persistentDataPath + "Image_" + _seq + ".png";
+        // _seq++;
+        // File.WriteAllBytes(path, encodedImageBytes);
+        // Camera.main.targetTexture = null;
+        // return imageBytes
+
     }
 
 
@@ -73,26 +107,26 @@ public class TestProtobuf : MonoBehaviour
     private Protocolor.Quaternion targetEstimatedPose;
     private TransformPosition targetEstimatedPosition;
     private TransformPosition droneEstimatedPosition;
-    public void StartPoseEstimation()
-    {
-        // cleanup agent and navmesh plane
-        agent.enabled = false;
-        planeObject = GameObject.Find("Plane");
-        Destroy(planeObject);
-
-        var rawBytes = CaptureScreenshot();
-        var poseEstimation = GetPoseEstimation(rawBytes);
-
-        Debug.Log("PoseEstimationData for drone quaternion: " +
-                  poseEstimation.DroneQuaternion.X + ", " + poseEstimation.DroneQuaternion.Y + ", " +
-                  poseEstimation.DroneQuaternion.Z +
-                  ", " + poseEstimation.DroneQuaternion.W);
-        droneEstimatedPose = poseEstimation.DroneQuaternion;
-        targetEstimatedPose = poseEstimation.TargetQuaternion;
-        targetEstimatedPosition = poseEstimation.TargetTransformPosition;
-        droneEstimatedPosition = poseEstimation.DroneTransformPosition;
-
-    }
+    // public void StartPoseEstimation()
+    // {
+    //     // cleanup agent and navmesh plane
+    //     agent.enabled = false;
+    //     planeObject = GameObject.Find("Plane");
+    //     Destroy(planeObject);
+    //
+    //     var rawBytes = CaptureScreenshot();
+    //     var poseEstimation = GetPoseEstimation(rawBytes);
+    //
+    //     Debug.Log("PoseEstimationData for drone quaternion: " +
+    //               poseEstimation.DroneQuaternion.X + ", " + poseEstimation.DroneQuaternion.Y + ", " +
+    //               poseEstimation.DroneQuaternion.Z +
+    //               ", " + poseEstimation.DroneQuaternion.W);
+    //     droneEstimatedPose = poseEstimation.DroneQuaternion;
+    //     targetEstimatedPose = poseEstimation.TargetQuaternion;
+    //     targetEstimatedPosition = poseEstimation.TargetTransformPosition;
+    //     droneEstimatedPosition = poseEstimation.DroneTransformPosition;
+    //
+    // }
 
     public void StartNavigation()
     {
@@ -105,14 +139,17 @@ public class TestProtobuf : MonoBehaviour
         // var estimatedRotation = Camera.main.transform.rotation * rotationObject;
         
         //Debug.Log("TargetPosition : " + target.transform.position);
-        var pos = new Vector3(targetEstimatedPosition.X, targetEstimatedPosition.Y, targetEstimatedPosition.Z);
+        var pos = new Vector3(targetEstimatedPosition.X, targetEstimatedPosition.Y, target.transform.localPosition.z);
 
         targetObject = GameObject.Find("TargetCube_modified");
         var targetPosition = Camera.main.transform.TransformPoint(pos);
         //Debug.Log("Estimated Target Position: " + targetPosition);
 
         droneObject = GameObject.Find("Drone_01 Variant_modified");
-        var dronePosition = Camera.main.transform.TransformPoint(new Vector3(droneEstimatedPosition.X, droneEstimatedPosition.Y, droneEstimatedPosition.Z));
+        var dronePosition = Camera.main.transform.TransformPoint(new Vector3(droneEstimatedPosition.X, droneEstimatedPosition.Y, drone.transform.localPosition.z));
+        
+        Debug.Log("Target Position : " + target.transform.position + "  Estimated Target Position: " + targetPosition);
+        Debug.Log("Drone Position : " + drone.transform.position + "  Estimated Drone Position: " + dronePosition);
 
         // Get the bottom of box collider y for drawing the plane correctly under the drone
         var droneCollider = droneObject.GetComponentInChildren<BoxCollider>();
@@ -125,19 +162,17 @@ public class TestProtobuf : MonoBehaviour
 
         // Get the top of box collider y for drawing the plane correctly over the target
         var targetCollider = targetObject.GetComponentInChildren<BoxCollider>();
-        var yHalfExtentsT = targetCollider.bounds.extents.y;
-        var yCenterT = targetCollider.bounds.center.y;
-        float yUpperT = transform.position.y + (yCenterT + yHalfExtentsT);
+        var yHalfExtentsT = targetCollider.bounds.extents.z;
+        var yCenterT = targetCollider.bounds.center.z;
+        float yUpperT = transform.position.z + (yCenterT + yHalfExtentsT);
         // set the y of target to the top of box collider
-        targetPosition.y = yUpperT;
+        targetPosition.z = yUpperT;
 
         // compare with ground truth
         var dronePositionGT = droneObject.transform.position;
-        dronePositionGT.y = yLower - offsetY;
+        dronePositionGT.z = yLower - offsetY;
         var targetPositionGT = targetObject.transform.position;
-        targetPosition.y = yUpperT;
-        Debug.Log("Target Position : " + targetPositionGT + "  Estimated Target Position: " + targetPosition);
-        Debug.Log("Drone Position : " + dronePositionGT + "  Estimated Drone Position: " + dronePosition);
+        targetPosition.z = yUpperT;
 
         // Do navmesh calculations with ground truth drone and target positions - comment out for pose estimation demo
         //targetPosition = targetPositionGT;
